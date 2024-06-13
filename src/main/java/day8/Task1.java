d7136a6f49917b037dff4ee9f8ee4e2f906a90c0
Ilya Krylov <pptestkrylov@gmail.com>
2024-06-13 10:51:55 +0300

    add day 8. Исправления

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -1,15 +1,34 @@
 package day8;
 
-public class Task1  {
-    public static void main (String[] args) {
-        StringBuilder string = new StringBuilder();
+public class Task1 {
+    public static void main(String[] args) {
+
+        String oldString = "0";
         long startTime = System.currentTimeMillis();
-        for (int i = 0; i<20000+1;i++){
-            string.append(i+" ");
+
+        for (int i = 1; i < 20000 + 1; i++) {
+           String addString = String.valueOf(i);
+           oldString=oldString+" "+addString;
         }
-        System.out.println(string.toString());
+        System.out.println(oldString);
         long stopTime = System.currentTimeMillis();
         long time = stopTime - startTime;
         System.out.println("Длительность работы в мс.: " + time);
+
+
+
+        StringBuilder string = new StringBuilder();
+        long newStartTime = System.currentTimeMillis();
+        for (int i = 0; i<20000+1;i++){
+            string.append(i+" ");
+        }
+        long newStopTime = System.currentTimeMillis();
+        long newTime = newStopTime - newStartTime;
+
+        System.out.println("Длительность работы в мс.: " + newTime);
+
+
     }
-}
+
+
+    }
